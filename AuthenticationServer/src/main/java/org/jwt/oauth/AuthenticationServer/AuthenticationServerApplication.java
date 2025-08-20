6c4b6ff653f010fff65796ef6f54af22ad4ad82a
csshivampatel <shivam.patel@confidosoft.in>
2025-08-20 18:51:09 +0530

    added tomcat dependency to deploy war file into it

--- AuthenticationServer/src/main/java/org/jwt/oauth/AuthenticationServer/AuthenticationServerApplication.java
+++ AuthenticationServer/src/main/java/org/jwt/oauth/AuthenticationServer/AuthenticationServerApplication.java
@@ -2,9 +2,16 @@ package org.jwt.oauth.AuthenticationServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AuthenticationServerApplication {
+public class AuthenticationServerApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AuthenticationServerApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthenticationServerApplication.class, args);
